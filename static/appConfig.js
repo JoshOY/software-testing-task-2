0d1c7771700675aa07e1daa56cf837db9cd28d89
Josh Ouyang <JoshOY789@gmail.com>
2016-05-18 11:04:31 +0800

    Ready for open source

--- static/appConfig.js
+++ static/appConfig.js
@@ -1,6 +1,6 @@
 const config = {
-  baseUrl: 'http://192.168.31.157:5001',
-  transactionUrl: 'http://192.168.31.157:5000',
+  baseUrl: 'http://192.168.43.142:5001',
+  transactionUrl: 'http://192.168.43.142:5000',
 };
 
 export default config;
